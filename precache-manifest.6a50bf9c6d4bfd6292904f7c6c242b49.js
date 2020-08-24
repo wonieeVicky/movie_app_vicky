cce8c1119fe7a12af41ca02d4c8d88366560a2fb
최혜원Vicky <fongfing@uneedcomms.com>
2020-08-24 21:33:23 +0900

    Updates

---
+++ precache-manifest.6a50bf9c6d4bfd6292904f7c6c242b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f8817f2e19e1c3c49e36d9b9b3941b",
+    "url": "/movie_app_vicky/index.html"
+  },
+  {
+    "revision": "2f0f4533ae2a5e1a0389",
+    "url": "/movie_app_vicky/static/css/main.26aff9e9.chunk.css"
+  },
+  {
+    "revision": "ab2f0e5696d7976ef5ef",
+    "url": "/movie_app_vicky/static/js/2.325bca26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_vicky/static/js/2.325bca26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f0f4533ae2a5e1a0389",
+    "url": "/movie_app_vicky/static/js/main.623b1144.chunk.js"
+  },
+  {
+    "revision": "79addfd7831263dc454d",
+    "url": "/movie_app_vicky/static/js/runtime-main.c87a7f36.js"
+  }
+]);
