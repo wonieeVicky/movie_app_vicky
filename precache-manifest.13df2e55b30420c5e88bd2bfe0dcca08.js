d22a94aaecdaa6c95d3da595d36eaa7dd531cff2
최혜원Vicky <fongfing@uneedcomms.com>
2020-08-27 23:07:02 +0900

    Updates

--- precache-manifest.13df2e55b30420c5e88bd2bfe0dcca08.js
+++ precache-manifest.13df2e55b30420c5e88bd2bfe0dcca08.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "04b68a87dec6ba54fd32e78f1e2a00b6",
+    "revision": "b0a8890432d05afdc541e04f08314a8a",
     "url": "/movie_app_vicky/index.html"
   },
   {
-    "revision": "6eb8c97ef9f4e4949321",
+    "revision": "3dac114a69ff664f5d40",
     "url": "/movie_app_vicky/static/css/main.697ed969.chunk.css"
   },
   {
-    "revision": "04383d98b1b10757966b",
-    "url": "/movie_app_vicky/static/js/2.69514038.chunk.js"
+    "revision": "0bb68131325223f16f7f",
+    "url": "/movie_app_vicky/static/js/2.2242420b.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/movie_app_vicky/static/js/2.69514038.chunk.js.LICENSE.txt"
+    "url": "/movie_app_vicky/static/js/2.2242420b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6eb8c97ef9f4e4949321",
-    "url": "/movie_app_vicky/static/js/main.74da6e6a.chunk.js"
+    "revision": "3dac114a69ff664f5d40",
+    "url": "/movie_app_vicky/static/js/main.d074dd9d.chunk.js"
   },
   {
     "revision": "79addfd7831263dc454d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_vicky/precache-manifest.74a65a11af2173082f0898e3d9faf7d3.js"
+  "/movie_app_vicky/precache-manifest.13df2e55b30420c5e88bd2bfe0dcca08.js"
 );
 
 self.addEventListener('message', (event) => {
