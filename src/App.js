6549ff4e00f85a397d4a852fb608abba15167451
최혜원Vicky <fongfing@uneedcomms.com>
2020-08-27 23:18:11 +0900

    feat: change method React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
@@ -9,12 +9,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navigation />
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
         <Route path="/movie/:id" component={Detail} />
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
